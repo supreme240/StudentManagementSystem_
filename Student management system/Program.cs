3de5ed0326663032e5b8fbd637ad88288f1c2b41
bsuyog1 <suyog.211241@ncit.edu.np>
2026-06-01 10:15:01 +0545

    made login as default(routing)

--- Student management system/Program.cs
+++ Student management system/Program.cs
@@ -38,7 +38,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=LogIn}/{action=LogIn}/{id?}")
     .WithStaticAssets();
 
 

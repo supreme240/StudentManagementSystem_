a74702fe359802397b43fa28e024c14b77e1a734
Dell <Dell@DESKTOP-DUJDL2E>
2026-06-05 15:50:25 +0545

    role model added

---
+++ StudentManagement.domain/Domain/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace StudentManagement.domain.Domain
+{
+    public class Roles
+    {
+        public int Id { get; set; }
+
+        public String Role {  get; set; } = string.Empty;
+    }
+}
--- StudentManagementSystem.Infrastructure/Data/ApplicationDbContext.cs
+++ StudentManagementSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace StudentManagementSystem.Infrastructure.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<Student> Students { get; set; } = null!;
         public DbSet<Registration> Registrations { get; set; } = null!;
+        public DbSet<Roles>Roles { get; set; } = null!;
     }
 }
 
